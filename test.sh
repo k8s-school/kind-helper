327a882b2af220cbad3226e1305e6b7ec98cc4dd
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-06-17 14:11:50 +0200

    rm test

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@
 set -e
 
 echo "Test nginx application"
-kubectl run --generator=run-pod/v1 -it --image=busybox shell wget http://my-nginx
+# kubectl run --generator=run-pod/v1 -it --image=busybox shell wget http://my-nginx
+# TODO try port forwad and curl
